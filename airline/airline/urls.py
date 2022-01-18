e47d6cdf7baaf6984a8f582101a593a8b14fe022
revelation136 <revelation136@gmail.com>
2022-01-19 00:22:39 +0800

    airline > flights > models.py (creating model)

--- airline/airline/urls.py
+++ airline/airline/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# don't forget include module (inherit it!)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('flights/', include('flights.urls'))
 ]
