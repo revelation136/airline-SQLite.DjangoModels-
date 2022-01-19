7d03526d6a7743acfdb5ab33c83cfa5f28a3835c
revelation136 <revelation136@gmail.com>
2022-01-20 03:12:34 +0800

    Shell - using python shell to add data to models.py

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -2,9 +2,23 @@ from django.db import models
 
 
 # Create your models here.
+# !!!Note:everytime you make changes in models.py, you have to make migrations and then migrate
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
 
 
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departure")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
+
+    # on_delete=models.CASCADE In this case, we specify that when an airport is deleted, all flights associated
+    # with it should be also deleted.
+    # related_name="" Which gives us a way to search for all flights with a given airport as their origin or destination
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
