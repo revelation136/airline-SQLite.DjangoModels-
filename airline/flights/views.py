d18a990d4c7eada7e30037a737c967fa3cb5c9f3
revelation136 <revelation136@gmail.com>
2022-01-20 15:58:54 +0800

    Starting our application - adding views, urls and templates. also you can query on db.sqlite3 on terminal

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Flight, Airport
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
