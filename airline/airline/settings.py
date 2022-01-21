e9df356c2962c221ff786789f22c3deafe829aff
revelation136 <revelation136@gmail.com>
2022-01-21 21:17:30 +0800

    Creating app - users

--- airline/airline/settings.py
+++ airline/airline/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Our Apps
-    'flights'
+    'flights',
+    'users',
 ]
 
 MIDDLEWARE = [
--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -6,9 +6,14 @@ class FlightAdmin(admin.ModelAdmin):
     list_display = ("id", "origin", "destination", "duration")
 
 
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+
+
 # Register your models here.
 admin.site.register(Flight, FlightAdmin)
+# register the flight but use the FlightAdmin interface.
 admin.site.register(Airport)
-admin.site.register(Passengers)
+admin.site.register(Passengers, PassengerAdmin)
 
 # user: jones, password: 1234, email: jones@a.com
